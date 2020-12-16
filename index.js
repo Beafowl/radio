fc23ae2c8213c9e991e709e6af9e10b4abd6e65a
bbuturak <berkbuturak@gmail.com>
2020-12-16 20:17:46 +0100

    Polished code

--- index.js
+++ index.js
@@ -3,39 +3,60 @@ const { token } = require('./config/config');
 
 const client = new Discord.Client();
 
-let audio;
+// outputs audio to bot
+let audioChannel;
+
+// plays audio to audiochannel
 let dispatcher;
+
+const printMessage = (msg) => {
+
+    const currentDate = new Date();
+
+    console.log(`[${currentDate.getHours()}:${currentDate.getMinutes()}:${currentDate.getSeconds()}] ${msg}`);
+
+}
 
 // function executed when online
 
-client.on("ready", () => {
-
-    console.log("Discord bot online");
+client.on('ready', () => {
+    printMessage('Discord bot online');
     client.user.setActivity('Bot für 89.0 RTL');
 });
 
-client.on("message", async (msg) => {
+
+client.on('message', async (msg) => {
 
     if (msg.author.bot) return;
     
-    if (msg.content == "!rtl") {
+    if (msg.content == '!rtl') { // bot joins channel and plays radio
 
 		if (msg.member.voice.channel) {
-            audio = await msg.member.voice.channel.join();
-            dispatcher = audio.play('https://stream.89.0rtl.de/live/mp3-128/direktlinkHP/');
 
-            dispatcher.on('start', () => {
-                console.log('audio.mp3 is now playing!');
-            });
-            
-            dispatcher.on('finish', () => {
-                console.log('audio.mp3 has finished playing!');
-            });
-            
-            dispatcher.on('error', console.error);
+            try {
+
+                audioChannel = await msg.member.voice.channel.join();
+                printMessage(`Channel ${msg.channel.name} at server ${msg.guild.name}: Bot joined voice channel ${msg.member.voice.channel}`);
+
+            } catch(err) {
+
+                console.log(err);
+            }
+            dispatcher = audioChannel.play('https://stream.89.0rtl.de/live/mp3-128/direktlinkHP/');
         }
 
+    } else if (msg.content == '!stop') { // stops music 
+
+        printMessage(`Channel ${msg.channel.name} at server ${msg.guild.name}: Bot has been stopped`);
+        dispatcher.end();
+
+    } else if (msg.content == '!quit') { // logout
+
+        printMessage('Bot logged out');
+        client.destroy();
     }
 });
 
 client.login(token);
+
+// link: https://discord.com/api/oauth2/authorize?client_id=788485025829486633&scope=bot&permissions=3145728
