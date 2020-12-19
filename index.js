9932a032050cf152538249c612b5e7a2ce470901
bbuturak <berkbuturak@gmail.com>
2020-12-19 01:17:20 +0100

    Bot leaves channel with no users, console prints log, refined code

--- index.js
+++ index.js
@@ -9,11 +9,24 @@ let audioChannel;
 // plays audio to audiochannel
 let dispatcher;
 
+// voice channel where the bot is located
+let voicechannel;
+let voicechannelId;
+
+// format number as two digits
+
+const formatNumber = (num) => {
+
+    return('0' + num).slice(-2);
+}
+
+// print message with timestamp
+
 const printMessage = (msg) => {
 
     const currentDate = new Date();
 
-    console.log(`[${currentDate.getHours()}:${currentDate.getMinutes()}:${currentDate.getSeconds()}] ${msg}`);
+    console.log(`[${formatNumber(currentDate.getHours())}:${formatNumber(currentDate.getMinutes())}:${formatNumber(currentDate.getSeconds())}] ${msg}`); // ("0" + myNumber).slice(-2);
 
 }
 
@@ -24,36 +37,50 @@ client.on('ready', () => {
     client.user.setActivity('Bot für 89.0 RTL');
 });
 
+// command received
 
 client.on('message', async (msg) => {
 
     if (msg.author.bot) return;
     
-    if (msg.content == '!rtl') { // bot joins channel and plays radio
+    if (msg.content == 'rtl start') { // bot joins channel and plays radio
 
 		if (msg.member.voice.channel) {
 
             try {
 
                 audioChannel = await msg.member.voice.channel.join();
-                printMessage(`Channel ${msg.channel.name} at server ${msg.guild.name}: Bot joined voice channel ${msg.member.voice.channel}`);
+                printMessage(`Channel ${msg.channel.name} at server ${msg.guild.name}: Bot joined voice channel ${msg.member.voice.channel.name}`);
 
             } catch(err) {
 
                 console.log(err);
             }
-            dispatcher = audioChannel.play('https://stream.89.0rtl.de/live/mp3-128/direktlinkHP/');
+            dispatcher = audioChannel.play('https://stream.89.0rtl.de/live/mp3-256/direktlinkHP/'); // 256 kbps
+            // https://stream.89.0rtl.de/live/mp3-128/direktlinkHP/ 128 kbps
         }
 
-    } else if (msg.content == '!stop') { // stops music 
+    } else if (msg.content == 'rtl stop') { // stops music, bot leaves voice channel
 
         printMessage(`Channel ${msg.channel.name} at server ${msg.guild.name}: Bot has been stopped`);
         dispatcher.end();
+        msg.member.voice.channel.leave();
+        voicechannel = undefined;
 
-    } else if (msg.content == '!quit') { // logout
+    }
+});
 
-        printMessage('Bot logged out');
-        client.destroy();
+// leave voice channel if no users are left
+
+client.on('voiceStateUpdate', (oldState, newState) => {
+
+    if (audioChannel) {
+
+        if (audioChannel.channel.members.size == 1) { // only the bot is in the voice channel; can leave it
+
+            printMessage(`Leaving ${audioChannel.channel.name}: No users`);
+            audioChannel.channel.leave();
+        }
     }
 });
 
